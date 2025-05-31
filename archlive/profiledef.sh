ab409b2ea684a1e183584b235cabe6acb788e916
Filippo <filippag07@proton.me>
2025-06-01 01:14:48 +0200

    added custom repo with paru

--- archlive/profiledef.sh
+++ archlive/profiledef.sh
@@ -3,7 +3,7 @@
 
 iso_name="archiso-custom"
 iso_label="ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
-iso_publisher="Arch Linux <https://archlinux.org>"
+iso_publisher="Tropical2497 <https://archlinux.org>"
 iso_application="Arch Linux Live/Rescue DVD"
 iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
 install_dir="arch"
